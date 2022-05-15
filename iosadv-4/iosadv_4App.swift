cccd4d346d20ba43d1c5d8a91dc12a1f28be8695
Dmitrii Lobanov <oftkor@gmail.com>
2022-05-16 01:04:35 +0300

    Initial Commit

---
+++ iosadv-4/iosadv_4App.swift
@@ -0,0 +1,17 @@
+//
+//  iosadv_4App.swift
+//  iosadv-4
+//
+//  Created by Dmitrii Lobanov on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct iosadv_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
